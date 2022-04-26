a0058e44bbfe2f070664bb64368e550da5e6c776
lorenzoferrini <lorenzo.ferrini@pal-robotics.com>
2022-04-26 18:30:18 +0200

    [WiP] Radar visual set up
    Radar visual set up currently consists of the aerial view of
    the fov and attention cones. Two spin boxes permit to change
    these cones amplitude. Currently resizing is not happening
    and there is visualization of either robots, people nor objects
    in the scene.

--- include/rqt_engagement_radar/RadarCanvas.hpp
+++ include/rqt_engagement_radar/RadarCanvas.hpp
@@ -5,10 +5,13 @@
 #include <QStringList>
 #include <QWidget>
 
+#include <QPen>
 #include <QColor>
 
 #include <QImage>
 #include <QSize>
+
+#include <QLine>
 
 namespace Ui {
 class RadarCanvas;
@@ -19,24 +22,40 @@ namespace rqt_engagement_radar {
 class RadarCanvas :
     public QWidget {
  Q_OBJECT
- public:
-  RadarCanvas(QWidget *parent = 0);
-  virtual ~RadarCanvas();
+    public:
+        RadarCanvas(QWidget *parent = 0);
+        virtual ~RadarCanvas();
+ 
+    public slots:
+        void fovConeDegChanged();
+        void attentionConeDegChanged();
 
- protected:
-  // Updates the scribble area where we are painting
-  void paintEvent(QPaintEvent *event) override;
+    protected:
+        // Updates the scribble area where we are painting
+        void paintEvent(QPaintEvent *event) override;
 
- private:
-  Ui::RadarCanvas *ui_;
+    private:
+        Ui::RadarCanvas *ui_;
 
-  // Holds the current pen width & color
-  int myPenWidth;
-  QColor myPenColor;
+        // Holds the current pen width & color
+        int myPenWidth;
+        QColor myPenColor;
+        QPen fovPen, attentionPen;
 
-  // Stores the image being drawn
-  QImage image;
+        // Stores the image being drawn
+        QImage image;
 
+        // Cones amplitude
+        double fovAmpl, attentionAmpl;
+
+        // Radar drawing components
+        double detectorLength;
+        double xOffset, yOffset;
+        double fovRectOriginX, fovRectOriginY; 
+        double fovX, fovY;
+        double attentionX, attentionY;
+        double fovStartAngle, fovSpanAngle; 
+        double attentionStartAngle, attentionSpanAngle;
 };
 
 } /* namespace */
--- src/rqt_engagement_radar/RadarCanvas.cpp
+++ src/rqt_engagement_radar/RadarCanvas.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
+
 #include "rqt_engagement_radar/RadarCanvas.hpp"
 
 #include "ui_radar_canvas.h"
 
 #include <QPainter>
+#include <QRectF>
+#include <cmath>
+
 
 namespace rqt_engagement_radar {
 
@@ -15,11 +20,21 @@ RadarCanvas::RadarCanvas(QWidget *parent) :
     ui_(new Ui::RadarCanvas()) {
   ui_->setupUi(this);
   
-  image = QImage(QSize(600, 600), QImage::Format_RGB32);
+  image = QImage(QSize(800, 600), QImage::Format_RGB32);
   image.fill(qRgb(255, 255, 255));
   
   QPainter painter(&image);
   painter.drawImage(QPoint(0, 0), image);
+
+  connect(ui_->fovConeDeg, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &RadarCanvas::fovConeDegChanged);
+  connect(ui_->attentionConeDeg, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &RadarCanvas::attentionConeDegChanged);
+
+  detectorLength = 400;
+  xOffset = 50; // Random value
+  yOffset = 300; // Image length (in this case 600) / 2 ==> 3000
+
+  fovPen = QPen(Qt::green, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+  attentionPen = QPen(Qt::red, 3, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
 
   update();
 }
@@ -30,12 +45,41 @@ RadarCanvas::~RadarCanvas() {
   delete ui_;
 }
 
+void RadarCanvas::fovConeDegChanged(){
+  fovAmpl = ui_->fovConeDeg->value(); 
+  update();
+}
+
+void RadarCanvas::attentionConeDegChanged(){
+  attentionAmpl = ui_->attentionConeDeg->value();
+  update();
+}
+
 void RadarCanvas::paintEvent(QPaintEvent *event){
   QPainter painter(this);
+  painter.setPen(fovPen);
 
-  // Draws the rectangle where the image needs to
-  // be updated
+  image = QImage(QSize(800, 600), QImage::Format_RGB32);
+  image.fill(qRgb(255, 255, 255));
+
   painter.drawImage(QPoint(0, 0), image);
+
+  fovRectOriginX = xOffset - detectorLength;
+  fovRectOriginY = yOffset - detectorLength;
+  fovStartAngle = -(floor(fovAmpl/2)*16 + ((fovAmpl-floor(fovAmpl/2))/100*16));
+  fovSpanAngle = floor(fovAmpl)*16 + ((fovAmpl-floor(fovAmpl))/100*16); 
+
+  QRectF fovRectangle(QPoint(fovRectOriginX, fovRectOriginY), QSize(detectorLength*2, detectorLength*2));
+  painter.drawPie(fovRectangle, fovStartAngle, fovSpanAngle);
+
+  painter.setPen(attentionPen);
+  attentionStartAngle = -(floor(attentionAmpl/2)*16 + ((attentionAmpl-floor(attentionAmpl/2))/100*16));
+  attentionSpanAngle = floor(attentionAmpl)*16 + ((attentionAmpl-floor(attentionAmpl))/100*16);
+
+  painter.drawPie(fovRectangle, attentionStartAngle, attentionSpanAngle);
+
+  //std::cout<<fovRectOriginX<<"\t"<<fovRectOriginY<<"\t"<<sin(fovAmpl/360*M_PI)<<std::endl;
+  //std::cout<<fovStartAngle<<"\t"<<fovSpanAngle<<"\t"<<std::endl;
 }
 
 } /* namespace */
